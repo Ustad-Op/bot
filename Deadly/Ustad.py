1893d3ae7ee4b26ba912b22a0823056518dace35
Ustad <73658726+Ustad-Op@users.noreply.github.com>
2022-01-13 20:28:38 +0530

    Update Ustad.py

--- Deadly/Ustad.py
+++ Deadly/Ustad.py
@@ -1,1 +1,36 @@
+from OpUstad import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, UstaD11, UstaD12, UstaD13, UstaD14, UstaD15, UstaD16, UstaD17, UstaD18, UstaD19, UstaD20
+ SUDO_USERS, DEV_USERS
+from telethon import events
+from time import time
+from datetime import datetime
 
+SMEX_USERS = []
+for x in SUDO_USERS:
+    SMEX_USERS.append(x)
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
