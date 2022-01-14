912f2833c0a76169f58a91e88d3b5ad668836fa9
Ustad <73658726+Ustad-Op@users.noreply.github.com>
2022-01-14 12:03:41 +0530

    Update Ustad.py

--- Deadly/Ustad.py
+++ Deadly/Ustad.py
@@ -1,4 +1,4 @@
-from OpUstad import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, UstaD11, UstaD12, UstaD13, UstaD14, UstaD15, UstaD16, UstaD17, UstaD18, UstaD19, UstaD20, SUDO_USERS, DEV_USERS
+from OpUstad import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, UstaD11, UstaD12, UstaD13, UstaD14, UstaD15, UstaD16, UstaD17, UstaD18, UstaD19, UstaD20, SUDO_USERS, DEV
 from OpUstad import HNDLR as hn
 from telethon import events
 from time import time
